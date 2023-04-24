aab63f0f9e3a0080de2db8599b15bf3bc81bf3b8
DiscovEar <tchindaphilippe@yahoo.fr>
2023-04-24 21:39:28 +0200

    Initial Commit

---
+++ brief.brief/brief_briefApp.swift
@@ -0,0 +1,17 @@
+//
+//  brief_briefApp.swift
+//  brief.brief
+//
+//  Created by Philippe Tchinda on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct brief_briefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
